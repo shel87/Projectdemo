efee93532e68d883659919cb75f50e2c34b29598
shel87 <shel87@gmail.com>
2015-04-18 13:57:22 -0400

    initial commit for demo1

---
+++ demo1/src/main/java/demo/Demo1Application.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+    	System.out.println("Hello World");
+        SpringApplication.run(Demo1Application.class, args);
+    }
+}
