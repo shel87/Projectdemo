672c15d030f2caac12895dc9ff03da55ab6e6e8d
Sheldon Gonsalves <shel87@gmail.com>
2015-05-09 10:15:46 -0400

    Update HelloController.java

--- demo1/src/main/java/demo/HelloController.java
+++ demo1/src/main/java/demo/HelloController.java
@@ -16,6 +16,6 @@ public class HelloController {
 	@RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, name));//takes name and id
     }
 }
